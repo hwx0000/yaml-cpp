26e4ed972196816cc12ef79a2eb296c5bb5c82ad
hwx0000 <wutong@papegames.net>
2022-08-09 13:19:41 +0800

    Add premake5.lua file

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+project "YAML_CPP"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.c"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
